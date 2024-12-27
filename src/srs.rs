3c3b5dbec35bfba583d6dc2a1e9218153e0e10b1
Andrei Ivasko <andrei@fantom.foundation>
2024-12-27 12:36:53 +0300

    refactor

--- src/srs.rs
+++ src/srs.rs
@@ -23,18 +23,24 @@ pub fn trusted_setup_generator(length: usize) -> SRSParams {
     // from a ceremony. Everyone can attend and give their random value and if
     // only one person destroys their toxic waste, all of the SRS will be safe to use.
     let toxic_waste = Fr::random(rng);
-    let mut trusted_setup_g1 = Vec::new();
-    let mut trusted_setup_g2 = Vec::new();
 
-    // x = toxic_waste
-    // SRS_g1 = g1 * x^0, g1 * x^1, g1 * x^2, g1 * x^3
-    // SRS_g2 = g2 * x^0, g2 * x^1, g2 * x^2, g2 * x^3
-    let mut toxic_waste_powers = Fr::ONE;
-    for _ in 0..length {
-        trusted_setup_g1.push((generator_g1 * toxic_waste_powers).to_affine());
-        trusted_setup_g2.push((generator_g2 * toxic_waste_powers).to_affine());
-        toxic_waste_powers *= toxic_waste;
-    }
+    let (trusted_setup_g1, trusted_setup_g2, _) = (0..length).fold(
+        (
+            Vec::with_capacity(length),
+            Vec::with_capacity(length),
+            Fr::ONE,
+        ),
+        |(mut acc_g1, mut acc_g2, current_power), _| {
+            acc_g1.push((generator_g1 * current_power).to_affine());
+            acc_g2.push((generator_g2 * current_power).to_affine());
+
+            // Update the power
+            let next_power = current_power * toxic_waste;
+
+            // Return the updated accumulator
+            (acc_g1, acc_g2, next_power)
+        },
+    );
 
     SRSParams {
         g1: trusted_setup_g1,
